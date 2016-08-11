fa8452a2eccfdacc79d221fe8c274712c7f4d7f4
mblackstock <mikeblackstock@gmail.com>
2016-08-10 17:53:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+sts-node-red-node-litedb
+========================
+
+This is a simple Node-Red node to read and write a single local sqlite database.
+
+This is based on the SQLite node - node-red-node-sqlite.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install sts-node-red-node-litedb --build-from-source
+
+The `--build-from-source' option will ensure the SQLite file size is limited and PRAGMA sql statements
+are disabled in SQLite.
+
+Usage
+-----
+
+Allows basic access to a single Sqlite database file.
+
+This node uses the <b>db.all</b> operation against the configured database. This does allow INSERTS, UPDATES and DELETES.
+
+Note that PRAGMA statements are NOT permitted; an error will be reported.
+
+By it's very nature it is SQL injection... so *be careful* out there...
+
+**msg.topic** must hold the <i>query</i> for the database, and the result is returned in **msg.payload**.
+
+Typically the returned payload will be an array of the result rows, (or an error).
+
+The reconnect timeout in milliseconds can be changed by adding a line to **settings.js**
+
+    litedbReconnectTime: 20000,
+
+The file to use for the single database (relative to the user directory) can be set by adding the following line to **settings.js**
+
+    litedbFileName: litedb
+
---
+++ litedb.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Portions Copyright (c) 2016 Sense Tecnic Systems, Inc.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var sqliteReconnectTime = RED.settings.litedbReconnectTime || 20000;
+    var liteDbFileName = RED.settings.litedbFileName || "litedb";
+    var sqlite3 = require('sqlite3');
+    console.log("loading litedb!");
+
+    var sqlitedb = null;
+    var connections = 0;
+    var connectionTimer = null;
+
+    // connect to sqlitedb if required
+    function dbConnect(node) {
+        if (sqlitedb === null) {
+            sqlitedb = new sqlite3.Database(RED.settings.userDir+"/"+liteDbFileName);
+            sqlitedb.on('open', function() {
+                node.log("opened litedb database ok");
+            });
+            sqlitedb.on('error', function(err) {
+                node.error("failed to open litedb database", err);
+                node.tick = setTimeout(function() { dbConnect(node); }, sqliteReconnectTime);
+            });           
+        }
+        // assume we will eventually connect
+        connections ++;
+    }
+
+    // close if needed
+    function dbClose(node) {
+        connections --;
+        if ((connections === 0) && sqlitedb) {
+            node.log("closing litedb database ok");
+            sqlitedb.close();
+            sqlitedb = null;
+            if (connectionTimer) {
+                clearTimeout(connectionTimer);
+            }
+        }
+    }
+
+    function SqliteNodeIn(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+
+        dbConnect(node);
+
+        node.on("input", function(msg) {
+            if (typeof msg.topic === 'string') {
+                //console.log("query:",msg.topic);
+                var bind = Array.isArray(msg.payload) ? msg.payload : [];
+                sqlitedb.all(msg.topic, bind, function(err, row) {
+                    if (err) { node.error(err,msg); }
+                    else {
+                        msg.payload = row;
+                        node.send(msg);
+                    }
+                });
+            }
+            else {
+                if (typeof msg.topic !== 'string') {
+                    node.error("msg.topic : the query is not a string",msg);
+                }
+            }
+        });
+        node.on("close", function() {
+            console.log("close called");
+            dbClose(node);
+        })
+    }
+    RED.nodes.registerType("litedb",SqliteNodeIn);
+}
